c5f658d4b46d63bc048dc1c03d8ac34ee3c378a9
Bmcalkins <b.raders1013@gmail.com>
2021-05-19 16:46:49 -0600

    new exp

--- examples.js
+++ examples.js
@@ -2,13 +2,23 @@
 //Two Number sum solution 
 function twoNumberSum(array, targetSum) {
     const nums = {};
-      for (const num of array) {
-          const potentialMatch = targetSum - num;
-          if (potentialMatch in nums) {
-              return [potentialMatch, num];
-          } else {
-              nums[num] = true;
-          }
-      }
-      return [];
-  }
+    for (const num of array) {
+        const potentialMatch = targetSum - num;
+        if (potentialMatch in nums) {
+            return [potentialMatch, num];
+        } else {
+            nums[num] = true;
+        }
+    }
+    return [];
+}
+//validate subsequence
+function isValidSubsequence(array, sequence) {
+	let sqIdx = 0; 
+	for (const value of array){
+ 		if (sqIdx === sequence.length) break;
+		if (sequence[sqIdx] === value) sqIdx++;
+	}
+	return sqIdx === sequence.length;
+}
+
